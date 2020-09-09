b0cbfcbcd51809acf2f090411ccae6a842f506d4
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-09-09 15:14:07 -0500

    add link for author and add author

--- routes/authors.js
+++ routes/authors.js
@@ -1,15 +1,47 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-
-router.get('/', (req, res) => {
-  res.render('authors/index');
+const Author = require("../models/author");
+// all authors route
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new ReqExp(req.query.name, "i");
+  }
+  try {
+    const authors = await Author.find({ searchOptions });
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
+  } catch {
+    res.redirect("/");
+  }
 });
-
-router.get('/new', (req, res) => {
-  res.render('auhtors/new');
+//New author route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
 });
-
-router.post('/', (req, res) => {
-  res.send('Create');
+// Create Author Route
+router.post("/", async (req, res) => {
+  const author = new Author({
+    name: req.body.name
+  });
+  try {
+    const newAuthor = await author.save();
+    res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating Author"
+    });
+  }
+  // author.save((err, newAuthor) => {
+  //   if (err) {
+  //     res.render("author/new", {
+  //       author: author,
+  //       errorMessage: "Error creating author"
+  //     });
+  //   } else {
+  //     // res.redirect(`author/${newAuthor.id}`)
+  //     res.redirect(`authors`);
+  //   }
+  // });
 });
 module.exports = router;
--- server.js
+++ server.js
@@ -1,28 +1,29 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const express = require('express');
+const express = require("express");
 const app = express();
-const expressLayouts = require('express-ejs-layouts');
+const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
-const indexRouter = require('./routes/index');
-const authorRouter = require('/routes/authors');
-
-app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views ');
-app.set('layout', 'layouts/layout');
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views ");
+app.set("layout", "layouts/layout");
 app.use(expressLayouts);
-app.use(express.static('public'));
-const mongoose = require('mongoose');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.on('open', () => console.log('Connected to mongoose'));
-app.use('/', indexRouter);
-app.use('/authors', authorRouter);
+db.on("error", error => console.error(error));
+db.on("open", () => console.log("Connected to mongoose"));
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 9000);

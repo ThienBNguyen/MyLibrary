acc00d836ea5c45ef0ffb20b013b42548623fcf0
Thien Nguyen <thien.nguyen30191@gmail.com>
2020-09-10 16:39:34 -0500

    file upload databaase use filepond

--- server.js
+++ server.js
@@ -1,30 +1,34 @@
-if (process.env.NODE_ENV !== "production") {
-  require("dotenv").config();
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
 }
-const express = require("express");
+const express = require('express');
 const app = express();
-const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require("body-parser");
-const indexRouter = require("./routes/index");
-const authorRouter = require("./routes/authors");
-const bookRouter = require("./routes/books");
+const expressLayouts = require('express-ejs-layouts');
+const bodyParser = require('body-parser');
 
-app.set("view engine", "ejs");
-app.set("views", __dirname + "/views ");
-app.set("layout", "layouts/layout");
+const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
+const bookRouter = require('./routes/books');
+
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.set('layout', 'layouts/layout');
 app.use(expressLayouts);
-app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
-const mongoose = require("mongoose");
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }));
+
+const mongoose = require('mongoose');
 mongoose.connect(process.env.DATABASE_URL, {
   useNewUrlParser: true,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
 
 const db = mongoose.connection;
-db.on("error", error => console.error(error));
-db.on("open", () => console.log("Connected to mongoose"));
-app.use("/", indexRouter);
-app.use("/authors", authorRouter);
-app.use("/book", bookRouter);
+db.on('error', (error) => console.error(error));
+db.once('open', () => console.log('Connected to Mongoose'));
+
+app.use('/', indexRouter);
+app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
+
 app.listen(process.env.PORT || 9000);
